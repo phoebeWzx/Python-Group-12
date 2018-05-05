4f99bec4d021d60c09a7339ac22b0c72761b414b
from-oz <winningshinydays@gmail.com>
2018-05-05 15:27:07 -0400

    Remove old data folders

--- DataFiles/datasources/developer-survey-2018/source/runner.sh
+++
@@ -1,7 +0,0 @@
-#install xlsxwrite lib to python
-cd XlsxWriter
-sudo python setup.py install
-#conbime all source data to the final
-cat *.csv > final.csv
-#convert it to xlsx file
-python convert.py
